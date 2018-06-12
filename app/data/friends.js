6fd89246898f7195d5ba762265c1569b53548121
Timothy Debien <timothydebien@Timothys-MacBook-Pro.local>
2018-06-11 18:17:22 -0600

    this is a final commit

---
+++ app/data/friends.js
@@ -0,0 +1,215 @@
+
+
+var friends = [
+    {
+        "name": "Ahmed",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    },
+    {
+        "name": "johny",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+        {
+        "name": "Jeremiah Scanlon",
+        "photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+        "scores": [
+            5,
+            2,
+            2,
+            2,
+            4,
+            1,
+            3,
+            2,
+            5,
+            5
+        ]
+    },
+    {
+        "name": "Louis T. Delia",
+        "photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+        "scores": [
+            3,
+            3,
+            4,
+            2,
+            2,
+            1,
+            3,
+            2,
+            2,
+            3
+        ]
+    },
+    {
+        "name": "Lou Ritter",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAkDAAAAJDhhZTI5NTk2LWQzZjUtNDJjZi1hMTM2LTQ3ZjNmYjE0YmY2NA.jpg",
+        "scores": [
+            4,
+            3,
+            4,
+            1,
+            5,
+            2,
+            5,
+            3,
+            1,
+            4
+        ]
+    },
+    {
+        "name": "Jordan Biason",
+        "photo": "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAAisAAAAJGUxYzc4YzA0LWQxMzUtNGI4NS04YTFiLTkwYzM0YTZkNzA2NA.jpg",
+        "scores": [
+            4,
+            4,
+            2,
+            3,
+            2,
+            2,
+            3,
+            2,
+            4,
+            5
+        ]
+    },
+    {
+        "name": "Tanner",
+        "photo": "https://nsidc.org/sites/nsidc.org/files/tanner.jpg",
+        "scores": [
+            1,
+            4,
+            4,
+            2,
+            3,
+            1,
+            1,
+            5,
+            3,
+            2
+        ]
+    },
+    {
+        "name": "testa",
+        "photo": "https://storage.googleapis.com/zopnow-static/images/products/320/fresh-apple-red-delicious-v-500-g.png",
+        "scores": [
+            4,
+            3,
+            4,
+            5,
+            2,
+            3,
+            5,
+            2,
+            2,
+            5
+        ]
+    },
+    {
+        "name": "John Sith",
+        "photo": "http://mlb.mlb.com/mlb/images/players/head_shot/501925.jpg",
+        "scores": [
+            4,
+            3,
+            3,
+            2,
+            2,
+            1,
+            3,
+            3,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Booger",
+        "photo": "https://i.imgur.com/8fsQstS.jpg",
+        "scores": [
+            4,
+            3,
+            3,
+            2,
+            2,
+            1,
+            3,
+            3,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "jerry",
+        "photo": "https://vignette.wikia.nocookie.net/rickandmorty/images/1/18/S1e10_jerry_touched.png/revision/latest?cb=20160911021834",
+        "scores": [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        "name": "morty",
+        "photo": "http://www.rickandmortyshow.com/wp-content/uploads/2016/06/Morty.png",
+        "scores": [
+            4,
+            3,
+            3,
+            2,
+            2,
+            1,
+            3,
+            3,
+            3,
+            5
+        ]
+    },
+    {
+        "name": "Rick",
+        "photo": "https://cdn.vox-cdn.com/thumbor/1kKyzwmocR6pu9ijSIl_l1XP0PY=/0x0:1280x720/1200x675/filters:focal(470x259:674x463)/cdn.vox-cdn.com/uploads/chorus_image/image/58089103/r_m_sauce.0.jpg",
+        "scores": [
+            4,
+            3,
+            3,
+            2,
+            2,
+            1,
+            3,
+            3,
+            3,
+            5
+        ]
+    }
+
+]
+
+module.exports = friends;
